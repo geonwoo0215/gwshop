42f5c338b82cb5bb58cada2bc5840ad92fa8e64c
Rose <82176176+geonwoo0215@users.noreply.github.com>
2022-08-17 03:29:29 +0900

    test : 로그인 테스트

--- src/test/java/com/GWShop/gwshop/service/MemberServiceTest.java
+++ src/test/java/com/GWShop/gwshop/service/MemberServiceTest.java
@@ -3,6 +3,7 @@ package com.GWShop.gwshop.service;
 import com.GWShop.gwshop.domain.Member;
 import com.GWShop.gwshop.repository.MemberRepository;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +19,11 @@ class MemberServiceTest {
 
     @Autowired
     private MemberService memberService;
+
+    @BeforeEach
+    private void clear() {
+        memberRepository.deleteAll();
+    }
 
     @Test
     @DisplayName("회원 저장")
@@ -40,5 +46,66 @@ class MemberServiceTest {
         Assertions.assertThat(saveMember.getPassword()).isEqualTo("12345678");
 
     }
+
+    @Test
+    @DisplayName("로그인")
+    void test2() {
+
+        //given
+        Member member = Member.builder()
+                .nickname("123")
+                .loginId("123456789")
+                .password("12345678")
+                .build();
+
+        //when
+        memberService.join(member);
+        Member loginMember = memberService.login(member.getLoginId(), member.getPassword());
+
+        //then
+        Assertions.assertThat(loginMember.getNickname()).isEqualTo("123");
+        Assertions.assertThat(loginMember.getLoginId()).isEqualTo("123456789");
+        Assertions.assertThat(loginMember.getPassword()).isEqualTo("12345678");
+
+    }
+
+    @Test
+    @DisplayName("아이디 오류")
+    void test3() {
+
+        //given
+        Member member = Member.builder()
+                .nickname("123")
+                .loginId("123456789")
+                .password("12345678")
+                .build();
+
+        memberService.join(member);
+
+        //expected
+        Assertions.assertThatThrownBy(() -> memberService.login("987654321", member.getPassword()))
+                .isInstanceOf(IllegalArgumentException.class).hasMessage("로그인 정보 시스템에 있는 계정과 일치하지 않습니다.");
+
+    }
+
+    @Test
+    @DisplayName("비밀번호 오류")
+    void test4() {
+
+        //given
+        Member member = Member.builder()
+                .nickname("123")
+                .loginId("123456789")
+                .password("12345678")
+                .build();
+
+        memberService.join(member);
+
+        //expected
+        Assertions.assertThatThrownBy(() -> memberService.login(member.getLoginId(), "987654321"))
+                .isInstanceOf(IllegalArgumentException.class).hasMessage("로그인 정보 시스템에 있는 계정과 일치하지 않습니다.");
+
+    }
+
 
 }
