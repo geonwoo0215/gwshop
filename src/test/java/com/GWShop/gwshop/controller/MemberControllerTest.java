894d0b6f4713328d858421d6f3238727c0ae4e39
Rose <82176176+geonwoo0215@users.noreply.github.com>
2022-08-17 20:54:30 +0900

    test : 로그인 컨트롤러 테스트

--- src/test/java/com/GWShop/gwshop/controller/MemberControllerTest.java
+++ src/test/java/com/GWShop/gwshop/controller/MemberControllerTest.java
@@ -1,6 +1,10 @@
 package com.GWShop.gwshop.controller;
 
+import com.GWShop.gwshop.controller.form.LoginForm;
 import com.GWShop.gwshop.controller.form.MemberJoinForm;
+import com.GWShop.gwshop.domain.Member;
+import com.GWShop.gwshop.repository.MemberRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -22,6 +26,9 @@ class MemberControllerTest {
 
     @Autowired
     private ObjectMapper objectMapper;
+
+    @Autowired
+    private MemberRepository memberRepository;
 
     @Test
     @DisplayName("가입 정보 정상 전달")
@@ -97,6 +104,63 @@ class MemberControllerTest {
                         .content(json))
                 .andExpect(MockMvcResultMatchers.status().isBadRequest())
                 .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    @DisplayName("회원 로그인")
+    void test5() throws Exception {
+
+        Member member = Member.builder()
+                .nickname("nickname")
+                .loginId("12345678")
+                .password("12345678")
+                .build();
+
+        memberRepository.save(member);
+
+
+        LoginForm loginForm = LoginForm.builder()
+                .loginId("12345678")
+                .password("12345678")
+                .build();
+
+        String json = objectMapper.writeValueAsString(loginForm);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/member/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print());
+
+
+    }
+    @Test
+    @DisplayName("회원 로그인 실패")
+    void test6() throws Exception {
+
+        Member member = Member.builder()
+                .nickname("nickname")
+                .loginId("12345678")
+                .password("12345678")
+                .build();
+
+        memberRepository.save(member);
+
+
+        LoginForm loginForm = LoginForm.builder()
+                .loginId("1234567")
+                .password("12345678")
+                .build();
+
+        String json = objectMapper.writeValueAsString(loginForm);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/member/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andDo(MockMvcResultHandlers.print());
+
+
     }
 
 
