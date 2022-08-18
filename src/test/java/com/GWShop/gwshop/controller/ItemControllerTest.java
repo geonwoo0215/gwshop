b847f0284c5c81ce3d7285da5a9385d9537dd830
Rose <82176176+geonwoo0215@users.noreply.github.com>
2022-08-18 19:49:28 +0900

    test : 상품 저장, 수정 테스트

---
+++ src/test/java/com/GWShop/gwshop/controller/ItemControllerTest.java
@@ -0,0 +1,48 @@
+package com.GWShop.gwshop.controller;
+
+import com.GWShop.gwshop.controller.form.ItemForm;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ItemControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    @DisplayName("상품 저장")
+    void test1() throws Exception {
+
+        ItemForm itemForm = ItemForm.builder()
+                .name("셔츠")
+                .price(10000)
+                .stock(10)
+                .build();
+
+        String json = objectMapper.writeValueAsString(itemForm);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/item")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print());
+
+    }
+}
